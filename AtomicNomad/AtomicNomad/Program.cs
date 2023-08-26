847e5f81f8d86be97b081fbb9112dd894d767bbc
MrScopes <drewmanrules23@gmail.com>
2023-08-26 10:23:13 -0400

    Initial Shell

---
+++ AtomicNomad/AtomicNomad/Program.cs
@@ -0,0 +1,23 @@
+﻿/**
+* 8/25/2023
+* CSC 253
+* Group **
+* Group Members: David Jones, Daniel Parks, Drew Tadlock
+* Mud Game for CSC 253. DUNGON CRAWLER MEETS ATOMIC DISASTER
+*/
+
+
+using System;
+using NomadLibrary;
+
+namespace AtomicNomad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Utilities.HelloWorld());
+            Console.Read();
+        }
+    }
+}
---
+++ AtomicNomad/NomadLibrary/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NomadLibrary
+{
+    public static class Utilities
+    {
+        public static string HelloWorld()
+        {
+            return "Hello, World! It is currently " + DateTime.Now.ToString("MM/dd/yyyy hh:mm tt") + ".";
+        }
+    }
+}
