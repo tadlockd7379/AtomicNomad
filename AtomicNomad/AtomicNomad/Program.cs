72320382f708e0dfd067ce23515aadbea1a531a4
Daniel Parks <adam.parks123@outlook.com>
2023-08-31 23:34:02 -0400

    Big update. Intro, Intro Menu

--- AtomicNomad/AtomicNomad/Program.cs
+++ AtomicNomad/AtomicNomad/Program.cs
@@ -5,8 +5,15 @@
 * Group Members: David Jones, Daniel Parks, Drew Tadlock
 * Mud Game for CSC 253. DUNGON CRAWLER MEETS ATOMIC DISASTER
 * 
-* dap160 - Daniel Adam Parks text clone/push
+* 
 */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using NomadLibrary;
 
 using AtomicNomad.game;
 
@@ -16,7 +23,85 @@ namespace AtomicNomad
     {
         static void Main(string[] args)
         {
-            new Game();
+
+            Title = "Atomic Nomad";
+            CursorVisible = false;
+
+
+            Intro IntroToGame = new Intro();
+            IntroToGame.GameIntro();
+
+            bool shouldExit = false;
+
+            do
+            {
+
+
+                string prompt = "";
+                string[] options = { " play ", " about", " exit " };
+                Menu mainMenu = new Menu(prompt, options);
+                int selectedIndex = mainMenu.RunMenu();
+
+
+                switch (selectedIndex)
+                {
+                    case 0:
+                        RunFirstOption();
+                        break;
+                    case 1:
+                        AboutInfo();
+                        break;
+                    case 2:
+                        ExitGame();
+                        break;
+                }
+
+                Clear();
+            } while (!shouldExit);
+
+
+
+
+            void ExitGame()
+            {
+                WriteLine("                                                   Press any key to exit...");
+                ReadKey(true);
+                Environment.Exit(0);
+            }
+
+
+
+            void AboutInfo()
+            {
+                Clear();
+                WriteLine(@"                                 
+
+
+                                           Some info about the group goes here.
+
+
+
+                   The path of the righteous man is beset on all sides by the inequities of the selfish 
+                   and the tyranny of evil men. Blessed is he who, in the name of charity and good will, 
+                   shepherds the weak through the valley of darkness, for he is truly his brother's keeper 
+                   and the finder of lost children. And I will strike down upon thee with great vengeance 
+                   and furious anger those who attempt to poison and destroy my brothers. And you will 
+                   know my name is the Lord when I lay my vengeance upon thee.
+
+
+                                               Press any key to continue...
+
+
+");
+                ReadKey();
+                Clear();
+
+            }
+            void RunFirstOption()
+            {
+                Clear();
+                new Game();
+            }
         }
     }
 }
---
+++ AtomicNomad/NomadLibrary/Menu.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace NomadLibrary
+{
+    public class Menu
+    {
+
+        public int SelectedIndex;
+        public string[] Options;
+        public string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+        }
+        private void DisplayOptions()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == SelectedIndex)
+                {
+                    prefix = "#";
+                    ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    prefix = " ";
+                    ForegroundColor = ConsoleColor.Gray;
+                }
+
+
+                WriteLine($"                                                   {prefix}    <<{currentOption} >>    {prefix} ");
+            }
+            ResetColor();
+        }
+        public int RunMenu()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                WriteLine(@"
+
+
+           
+
+
+             █████╗ ████████╗ ██████╗ ███╗   ███╗██╗ ██████╗    ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ██████╗ 
+            ██╔══██╗╚══██╔══╝██╔═══██╗████╗ ████║██║██╔════╝    ████╗  ██║██╔═══██╗████╗ ████║██╔══██╗██╔══██╗
+            ███████║   ██║   ██║   ██║██╔████╔██║██║██║         ██╔██╗ ██║██║   ██║██╔████╔██║███████║██║  ██║
+            ██╔══██║   ██║   ██║   ██║██║╚██╔╝██║██║██║         ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║  ██║
+            ██║  ██║   ██║   ╚██████╔╝██║ ╚═╝ ██║██║╚██████╗    ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██████╔╝
+            ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝ ╚═════╝    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ 
+                                                                                                  
+
+
+            
+                                          Use the ARROW keys to select your option.
+                                            Press ENTER to finalize your choice.
+
+
+
+");
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+
+
+
+            return SelectedIndex;
+        }
+
+
+
+    }
+
+
+}
