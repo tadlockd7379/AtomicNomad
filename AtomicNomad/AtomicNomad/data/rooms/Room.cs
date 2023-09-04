b6fc4824908ce9690998f3279a30baa53320711c
Daniel Parks <adam.parks123@outlook.com>
2023-09-03 22:22:07 -0400

    Added Rooms.json Program works with build error.

---
+++ AtomicNomad/AtomicNomad/data/rooms/Room.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class Room
+{
+    public int RoomID { get; set; }
+    public string RoomName { get; set; }
+    public string RoomDescription { get; set; }
+    public List<string> Items { get; set; }
+    public List<string> Enemies { get; set; }
+}
--- AtomicNomad/AtomicNomad/game/Game.cs
+++ AtomicNomad/AtomicNomad/game/Game.cs
@@ -1,11 +1,9 @@
-﻿/*
- * The main actual game class, made it seperate from Program.cs so it's not static.
-*/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using AtomicNomad.game.items;
 using NomadLibrary;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace AtomicNomad.game
 {
@@ -14,17 +12,20 @@ namespace AtomicNomad.game
         public static RNG RNG = new RNG();
         public static Items Items = new Items();
         public static Dictionary<string, string> Keywords = new Dictionary<string, string>();
+        public static List<Room> RoomsData = new List<Room>();
 
         public Game()
         {
             Console.WriteLine("Welcome to AtomicNomad!\n");
+
+            LoadRooms(@"..\..\..\..\..\..\..\..\data\rooms\Rooms.json");
 
             RegisterKeywords();
 
             Items.Weapons["sword"].Attack();
             Console.WriteLine(Items["arrow"].Description);
 
-            Console.WriteLine("\ntemporary user input system... try 'items'");
+            Console.WriteLine("\ntemporary user input system... try 'items' or 'rooms'");
 
             Input();
         }
@@ -34,14 +35,14 @@ namespace AtomicNomad.game
             Console.WriteLine("");
             string input = Console.ReadLine().ToLower();
             Console.WriteLine("");
-            
-            Keywords.TryGetValue(input, out string value);
-            if (value != null)
+
+            if (Keywords.TryGetValue(input, out string value))
             {
                 Console.WriteLine(value);
-            } else
+            }
+            else
             {
-                Console.WriteLine($"Unknown input '{input}' Try 'items'.");
+                Console.WriteLine($"Unknown input '{input}' Try 'items' or 'rooms'.");
             }
 
             Input();
@@ -51,7 +52,7 @@ namespace AtomicNomad.game
         {
             Keywords.Add("items",
                 $"There are currently {Items.Miscellaneous.Count + Items.Potions.Count + Items.Weapons.Count} total items.\n" +
-                $"{Items.Weapons.Count} weapons, {Items.Potions.Count} potions, and {Items.Miscellaneous.Count} miscalaneous items.\n" +
+                $"{Items.Weapons.Count} weapons, {Items.Potions.Count} potions, and {Items.Miscellaneous.Count} miscellaneous items.\n" +
                 "Try 'weapons', 'potions', or 'misc'."
             );
 
@@ -64,7 +65,32 @@ namespace AtomicNomad.game
             Keywords.Add("misc", string.Join(", ", Items.Miscellaneous.Keys));
             foreach (var data in Items.Miscellaneous) Keywords.Add(data.Key, $"{data.Value.ID} is the {data.Value.Description}");
 
+            Keywords.Add("rooms", "List of available rooms:\n" + GetRoomList());
+
             Console.WriteLine($"Registered {Keywords.Count} total keywords.\n");
+        }
+
+        string GetRoomList()
+        {
+            List<string> roomNames = new List<string>();
+            foreach (var room in RoomsData)
+            {
+                roomNames.Add(room.RoomName);
+            }
+            return string.Join(", ", roomNames);
+        }
+
+        void LoadRooms(string jsonFilePath)
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(jsonFilePath);
+                RoomsData = JsonConvert.DeserializeObject<List<Room>>(jsonContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading rooms data: " + ex.Message);
+            }
         }
     }
 }
